64555df781432443f7cce644c21ff8443d911d93
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2022-12-06 16:27:29 -0500

    Realese v1.0

--- src/authApp/serializers/tostionSerializer.py
+++ src/authApp/serializers/tostionSerializer.py
@@ -4,4 +4,4 @@ from authApp.models.tostion import Tostion
 class TostionSerializer (serializers.ModelSerializer):
     class Meta:
         model = Tostion
-        fields = ['fecha', 'cantidadEntra', 'cantidadSale', 'tempInicio','temp1', 'temp2', 'tiempo1', 'tiempo2', 'tempPromedio', 'tiempoPromedio', 'aromas', 'agtron', 'lote', 'user']
+        fields = ['fecha', 'cantidadEntra', 'cantidadSale', 'tempInicio','temp1', 'temp2', 'tiempo1', 'tiempo2', 'tempPromedio', 'tiempoPromedio', 'aromas', 'agtron', 'lote', 'user', 'maquina']
