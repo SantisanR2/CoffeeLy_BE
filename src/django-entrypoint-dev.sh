7525808811481c9e18308db9cbe9ea689edf08c0
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2022-12-06 16:21:04 -0500

    Realese v1.0

--- src/django-entrypoint-dev.sh
+++ src/django-entrypoint-dev.sh
@@ -5,6 +5,7 @@ while ! nc -z postgres 5432; do
 done
 
 echo "PostgreSQL started"
+python manage.py makemigrations authApp
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py runserver 0.0.0.0:8000
